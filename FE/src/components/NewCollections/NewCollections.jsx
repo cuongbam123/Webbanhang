fce1dd739cf94874c67aa01deea027c53c22cbe4
tdatnef <phamdatqn2006@gmail.com>
2025-06-26 14:26:14 +0700

    popular - new collection

--- FE/src/components/NewCollections/NewCollections.jsx
+++ FE/src/components/NewCollections/NewCollections.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
-import './NewCollections.css'
-import new_collections from '../Assets/new_collections'
-import Item from '../Item/Item'
+import React, { useContext } from "react";
+import "./NewCollections.css";
+import { ShopContext } from "../../Context/ShopContext";
+import { Link } from "react-router-dom";
 
 const NewCollections = () => {
-    return (
-        <div className = 'new-collections'>
-            <h1>NEW COLLECTIONS</h1>
-            <hr />
-            <div className="collections">
-                {new_collections.map((item,i)=>{
-                    return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-                })}
+  const { all_product } = useContext(ShopContext);
+  // Lấy 8 sản phẩm mới nhất (giả sử backend trả về sản phẩm mới nhất ở cuối mảng)
+  const newProducts = all_product.slice(-8).reverse();
+  return (
+    <div className="new-collections">
+      <h1>NEW COLLECTIONS</h1>
+      <hr />
+      <div className="collections product-list">
+        {newProducts.map((product, i) => (
+          <div key={product._id || i} className="product-card">
+            <Link to={`/product/${product._id}`}>
+              <img src={product.image} alt={product.name_product} />
+            </Link>
+            <div className="product-info">
+              <h3>{product.name_product}</h3>
+              <p>
+                {product.price_product &&
+                  product.price_product.toLocaleString()}{" "}
+                VNĐ
+                {product.old_price && (
+                  <span className="old-price" style={{ marginLeft: 8 }}>
+                    {product.old_price.toLocaleString()} VNĐ
+                  </span>
+                )}
+              </p>
             </div>
-        </div>
-    )
-}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default NewCollections
+export default NewCollections;
--- FE/src/components/Popular/Popular.jsx
+++ FE/src/components/Popular/Popular.jsx
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Popular.css";
-import data_product from "../Assets/data";
+import { ShopContext } from "../../Context/ShopContext";
 import Item from "../Item/Item";
 
 const Popular = () => {
+  const { all_product } = useContext(ShopContext);
+  // Lấy 4 sản phẩm có giá cao nhất
+  const popularProducts = [...all_product]
+    .sort((a, b) => Number(b.price_product) - Number(a.price_product))
+    .slice(0, 4);
   return (
     <div className="popular">
       <h1>POPULAR DEVICES</h1>
       <hr />
       <div className="popular-item">
-        {data_product.map((item, i) => {
-          return (
-            <Item
-              key={i}
-              id={item.id}
-              name={item.name}
-              image={item.image}
-              new_price={item.new_price}
-              old_price={item.old_price}
-            />
-          );
-        })}
+        {popularProducts.map((item, i) => (
+          <Item
+            key={item._id || i}
+            id={item._id}
+            name={item.name_product}
+            image={item.image}
+            new_price={item.price_product}
+            old_price={item.old_price}
+          />
+        ))}
       </div>
     </div>
   );
